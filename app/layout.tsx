7bbbc5958e5cdf6ae5c9dc5694c43f21518906ba
Nikita Pandey <nikitapandey606@gmail.com>
2025-11-27 18:26:51 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "MyAI3",
-  description: "MyAI3",
+  title: "Bingio",
+  description: "Your emotionally intelligent movie assistant",
 };
 
 export default function RootLayout({
